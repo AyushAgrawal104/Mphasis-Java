d0f7cb2703af56f1e9996e497e93e6ba04d89871
richardsforu <praveen.somireddy@gmail.com>
2017-08-28 14:23:28 +0530

    JUnit added

--- JUnit/JUnitTet/src/com/TestJunit.java
+++ JUnit/JUnitTet/src/com/TestJunit.java
@@ -2,6 +2,10 @@ package com;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestJunit{
@@ -9,9 +13,30 @@ public class TestJunit{
 	String message = "Hello";
 	MessageUtil messageUtil = new MessageUtil(message);
 
+	
+	@AfterClass()
+	public static void f3() {
+		System.out.println("--- Before class method");
+	}
+	
+	@BeforeClass()
+	public static void f4() {
+		System.out.println("--- After class  method");
+	}
 
-	@Test
+	@After()
+	public void f1() {
+		System.out.println("--- after f1 method");
+	}
+	
+	@Before
+	public void f2() {
+		System.out.println("--- before f1 method");
+	}
+	
+	@
 	public void testPrintMessage() {
+		
 		
 		assertEquals("Hello", messageUtil.printMessage());
 	}
