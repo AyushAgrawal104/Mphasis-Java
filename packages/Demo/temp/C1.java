eced884e66d27d1c21ca2071a45b96a51df6fe56
richardsforu <praveen.somireddy@gmail.com>
2017-08-28 21:14:21 +0530

    JUnit added

--- packages/Demo/temp/C1.java
+++
@@ -1,8 +0,0 @@
-package arth;
-public class C1{
-
-public void f1(){
-   System.out.println("--- Testting");
-}
-
-}
--- packages/sp/Test.java
+++
@@ -1,9 +0,0 @@
-class Test{
-
-public static void main(String[] args){
-
-   Employee emp=new Employee();
-   emp.f1();
-}
-
-}
