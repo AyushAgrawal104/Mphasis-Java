6ccb8536dcea7ba50544f64a8f7801b58b9b8d50
richardsforu <praveen.somireddy@gmail.com>
2017-09-11 12:25:11 +0530

    Java Script  added

---
+++ Java Script/JS-Intro/WebContent/1-Intro.js
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+
+// strings
+var name = "Nicholas";
+var selection = "a";
+// numbers
+var count = 25;
+var cost = 1.51;
+// boolean
+var found = true;
+// null
+var object = null;
+// undefined
+var flag = undefined;
+var ref; // assigned undefined automatically
+
+//console.log(typeof "Nicholas"); // "string"
+//console.log(typeof 10); // "number"
+//console.log(typeof 5.1); // "number"
+//console.log(typeof false); // "boolean"
+//console.log(typeof undefined); // "undefined"
+//console.log(typeof null);
+/*var emp=null;
+var emp1;
+console.log(emp1 === null); */
+
+/*var name="Abc";
+
+console.log("5"==5);
+console.log("5"===5);*/
+
+/*
+var object1 = new Object();
+console.log(typeof object1);
+var obj;
+console.log(typeof obj);
+obj=object1;
+
+console.log(typeof obj);*/
+
+/*var emp=new Object();
+emp.empId=10;
+emp.empName='Praveen';
+emp.salary=76334;
+
+console.log(emp);
+console.log("EmpId : "+emp.empId);
+console.log("Emp Name : "+emp.empName);
+console.log("Emp Salary : "+emp.salary);*/
+
+
+var emp={
+		empId:'1024',
+		empName:'Praveen Reddy S',
+		salary:99733
+}
+
+console.log("Employee: "+emp);
+console.log("Id: "+emp.empId);
+console.log("Name: "+emp.empName);
+console.log("Salary: "+emp.salary);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
